b157f6b8d669cfbeb161faccdf295c27486a057c
bdring <barton.dring@gmail.com>
2022-07-26 14:49:35 -0500

    WIP

--- FluidNC/src/Kinematics/ParallelDelta.cpp
+++ FluidNC/src/Kinematics/ParallelDelta.cpp
@@ -8,6 +8,8 @@
 
 #include <cmath>
 
+
+
 /*
 Default configuration
 
@@ -16,19 +18,127 @@ kinematics:
 
   TODO 
    - Constrain the geometry values to realistic values.
+   - Implement $MI for dynamixel motors.
 
 */
 
 namespace Kinematics {
+
+    // trigonometric constants to speed up calculations
+    const float sqrt3  = 1.732050807;
+    const float dtr    = M_PI / (float)180.0;  // degrees to radians
+    const float sin120 = sqrt3 / 2.0;
+    const float cos120 = -0.5;
+    const float tan60  = sqrt3;
+    const float sin30  = 0.5;
+    const float tan30  = 1.0 / sqrt3;
+
+    // the geometry of the delta
+    float rf;  // radius of the fixed side (length of motor cranks)
+    float re;  // radius of end effector side (length of linkages)
+    float f;   // sized of fixed side triangel
+    float e;   // size of end effector side triangle
+
     void ParallelDelta::group(Configuration::HandlerBase& handler) {
-        handler.item("crank_mm", _crank_length);
-        handler.item("base_triangle_mm", _base_triangle);
-        handler.item("linkage_mm", _linkage_length);
-        handler.item("end_effector_triangle_mm", _end_effector_triangle_mm);
+        handler.item("crank_mm", _crank_length, 50.0, 500.0);
+        handler.item("base_triangle_mm", _base_triangle, 20.0, 500.0);
+        handler.item("linkage_mm", _linkage_length, 20.0, 500.0);
+        handler.item("end_effector_triangle_mm", _end_effector_triangle_mm, 20.0, 500.0);
+
+        handler.item("max_negative_angle_rad", _max_negative_angle, -(M_PI / 2.0), 0.0); // max angle up the arm can safely go
+        handler.item("max_positive_angle_rad", _max_positive_angle, 0.0, (M_PI / 2.0));  // max_angle down the arm can safely go
+
+        rf = _crank_length;                   // radius of the fixed side (length of motor cranks)
+        re = _base_triangle;                  // radius of end effector side (length of linkages)
+        f  = _base_triangle;                  // sized of fixed side triangel
+        e  = _end_effector_triangle_mm;       // size of end effector side triangle
     }
 
     void ParallelDelta::init() {
+        float angles[MAX_N_AXIS]    = { 0.0, 0.0, 0.0 };
+        float cartesian[MAX_N_AXIS]  = { 0.0, 0.0, 0.0 };
+
+        // Calculate the Z offset at the arm zero angles ...
+        // Z offset is the z distance from the motor axes to the end effector axes at zero angle
+        motors_to_cartesian(cartesian, angles, 3);  // Sets the cartesian values
+
+        // print a startup message to show the kinematics are enabled. Print the offset for reference
         log_info("Kinematic system: " << name());
+        log_info("  Z Offset:" << cartesian[Z_AXIS] << " Max neg angle:" << _max_negative_angle << " Max pos angle:" << _max_positive_angle);
+
+        
+        //     grbl_msg_sendf(CLIENT_SERIAL,
+        //                    MsgLevel::Info,
+        //                    "DXL_COUNT_MIN %4.0f CENTER %d MAX %4.0f PER_RAD %d",
+        //                    DXL_COUNT_MIN,
+        //                    DXL_CENTER,
+        //                    DXL_COUNT_MAX,
+        //                    DXL_COUNT_PER_RADIAN);
+
+    }
+
+    bool ParallelDelta::cartesian_to_motors(float* target, plan_line_data_t* pl_data, float* position) {
+        // Motor space is cartesian space, so we do no transform.
+        return mc_move_motors(target, pl_data);
+    }
+
+    void ParallelDelta::motors_to_cartesian(float* cartesian, float* motors, int n_axis) {
+        //read_settings();
+        //log_info("motors_to_cartesian motors: (" << motors[0] << "," << motors[1] << "," << motors[2] << ")");
+
+        float t = (f - e) * tan30 / 2;
+        //log_info("  t:" << t);
+
+        float y1 = -(t + rf * cos(motors[0]));
+        float z1 = -rf * sin(motors[0]);
+        //log_info("  y1:" << y1 << " z1:" << z1);
+
+        float y2 = (t + rf * cos(motors[1])) * sin30;
+        float x2 = y2 * tan60;
+        float z2 = -rf * sin(motors[1]);
+        //log_info("  y2:" << y2 << " x2:" << x2 << " z2:" << z2);
+
+        float y3 = (t + rf * cos(motors[2])) * sin30;
+        float x3 = -y3 * tan60;
+        float z3 = -rf * sin(motors[2]);
+        //log_info("  y3:" << y3 << " x3:" << x3 << " z3:" << z3);
+
+        float dnm = (y2 - y1) * x3 - (y3 - y1) * x2;
+        //log_info("  dnm:" << dnm);
+
+        float w1 = y1 * y1 + z1 * z1;
+        float w2 = x2 * x2 + y2 * y2 + z2 * z2;
+        float w3 = x3 * x3 + y3 * y3 + z3 * z3;
+        //log_info("  w1:" << w1 << " w2:" << w2 << " w3:" << w3);
+
+        // x = (a1*z + b1)/dnm
+        float a1 = (z2 - z1) * (y3 - y1) - (z3 - z1) * (y2 - y1);
+        float b1 = -((w2 - w1) * (y3 - y1) - (w3 - w1) * (y2 - y1)) / 2.0;
+        //log_info("  a1:" << a1 << " b1:" << b1);
+
+        // y = (a2*z + b2)/dnm;
+        float a2 = -(z2 - z1) * x3 + (z3 - z1) * x2;
+        float b2 = ((w2 - w1) * x3 - (w3 - w1) * x2) / 2.0;
+        //log_info("  a2:" << a2 << " b2:" << b2);
+
+        // a*z^2 + b*z + c = 0
+        float a = a1 * a1 + a2 * a2 + dnm * dnm;
+        float b = 2 * (a1 * b1 + a2 * (b2 - y1 * dnm) - z1 * dnm * dnm);
+        float c = (b2 - y1 * dnm) * (b2 - y1 * dnm) + b1 * b1 + dnm * dnm * (z1 * z1 - re * re);
+        //log_info("  c:" << c << " b:" << b << " c:" << c);
+
+        // discriminant
+        float d = b * b - (float)4.0 * a * c;
+        //log_info("  d:" << d);
+        if (d < 0) {
+            log_warn("Forward Kinematics Error");
+            return;
+        }
+
+        //log_info("  a:" << a << " b:" << b << " d:" << d);
+        cartesian[Z_AXIS] = -(float)0.5 * (b + sqrt(d)) / a;
+        cartesian[X_AXIS] = (a1 * cartesian[Z_AXIS] + b1) / dnm;
+        cartesian[Y_AXIS] = (a2 * cartesian[Z_AXIS] + b2) / dnm;
     }
 
     // Configuration registration
--- FluidNC/src/Kinematics/ParallelDelta.h
+++ FluidNC/src/Kinematics/ParallelDelta.h
@@ -12,6 +12,16 @@
 #include "Kinematics.h"
 #include "Cartesian.h"
 
+// M_PI is not defined in standard C/C++ but some compilers
+// support it anyway.  The following suppresses Intellisense
+// problem reports.
+#ifndef M_PI
+#    define M_PI 3.14159265358979323846
+#endif
+
+#define ARM_INTERNAL_ANGLE 0.05  // radians 2.866°  // due to mounting angle
+#define DXL_CENTER 2015          // (DXL_COUNTS / 2) - (ARM_INTERNAL_ANGLE * DXL_COUNT_PER_RADIAN)
+
 namespace Kinematics {
     class ParallelDelta : public Cartesian {
     public:
@@ -26,8 +36,8 @@ namespace Kinematics {
         virtual void init() override;
         // bool         kinematics_homing(AxisMask cycle_mask) override;
         // void         kinematics_post_homing() override;
-        // bool         cartesian_to_motors(float* target, plan_line_data_t* pl_data, float* position) override;
-        // void         motors_to_cartesian(float* cartesian, float* motors, int n_axis) override;
+        bool cartesian_to_motors(float* target, plan_line_data_t* pl_data, float* position) override;
+        void motors_to_cartesian(float* cartesian, float* motors, int n_axis) override;
 
         // Configuration handlers:
         void         validate() const override {}
@@ -35,16 +45,19 @@ namespace Kinematics {
         void         afterParse() override {}
 
         // Name of the configurable. Must match the name registered in the cpp file.
-        virtual const char* name() const override { return "ParallelDelta"; }
+        virtual const char* name() const override { return "parallel_delta"; }
 
         ~ParallelDelta() {}
 
     private:
         //  Config items
-        float _crank_length             = 70;  // The length of the crank arm on the motor
-        float _base_triangle            = 45;
-        float _linkage_length           = 100;
-        float _end_effector_triangle_mm = 100;
+        float _crank_length;  // The length of the crank arm on the motor
+        float _base_triangle;
+        float _linkage_length;
+        float _end_effector_triangle_mm;
+
+        float _max_negative_angle = (M_PI / 3.0);  // default is 60 Deg up
+        float _max_positive_angle = (M_PI / 2.0);  // default is 90 Deg down
 
     protected:
     };
